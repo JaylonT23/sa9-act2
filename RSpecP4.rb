4c79debaa69177c5498504cc207f7d13804906b3
Jaylon Taylor <jaylon.taylor2000@gmail.com>
2024-03-15 03:58:08 -0500

    Edit the rspecFiles

---
+++ RSpecP4.rb
@@ -0,0 +1,37 @@
+  RSpec.describe BankAccount do
+  describe "#deposit" do
+    it "increases the balance by the deposit amount" do
+      account = BankAccount.new
+      account.deposit(100)
+      expect(account.balance).to eq(100)
+    end
+  end
+
+  describe "#withdraw" do
+    context "with sufficient funds" do
+      it "decreases the balance by the withdrawal amount" do
+        account = BankAccount.new
+        account.deposit(100)
+        account.withdraw(50)
+        expect(account.balance).to eq(50)
+      end
+    end
+
+    context "with insufficient funds" do
+      it "does not change the balance" do
+        account = BankAccount.new
+        account.deposit(100)
+        account.withdraw(150)
+        expect(account.balance).to eq(100)
+      end
+    end
+  end
+
+  describe "#balance" do
+    it "returns the current balance" do
+      account = BankAccount.new
+      account.deposit(100)
+      expect(account.balance).to eq(100)
+    end
+  end
+end
